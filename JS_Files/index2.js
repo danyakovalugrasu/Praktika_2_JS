b6536191c56ae3dcbddc4854fd33cd42832bbe7f
danyakovalugrasu <danyakovalugrasu@gmail.com>
2023-10-14 00:52:15 +0500

    lab2js

--- JS_Files/index2.js
+++ JS_Files/index2.js
@@ -25,17 +25,31 @@ document.querySelector("#button_part1").onclick = function () {
 document.querySelector("#button_part2").onclick = function () {
     var result2 = document.getElementById("result2");
     const matrix_result = [];
+    var temp = false;
     result = "";
-    for (var i = 1; i <= 5; i++)
+    for (var i = 1; i <= 10; i++)
     {
         matrix_result[i] = [];
-        for (var j = 1; j <= 6; j++)
+        for (var j = 1; j <= 10; j++)
         {
             matrix_result[i][j] = i * j;
             result += matrix_result[i][j];
             result += " ";
+            if (j == 6)
+            {
+                temp = true;
+                break;
+            }
         }
         result += "\n";
+        if (i == 5 && temp == true)
+        {
+            break
+        }
+        else
+        {
+            temp = false;
+        }
     }
     result2.innerText = result;
     alert("Работа завершена")
@@ -47,7 +61,7 @@ document.querySelector("#button_part3").onclick = function () {
     var result3 = document.getElementById("result3");
     var result3_2 = document.getElementById("result3_2");
     result = "";
-    for (var i = 1; i <= 5; i++)
+    for (var i = 1; i <= 3; i++)
     {
         arr[i] = Math.floor(Math.random() * 100);
         result += arr[i];
@@ -118,15 +132,233 @@ document.querySelector("#button_part5").onclick = function () {
     }
 }
 
+// 6
+document.querySelector("#button_part6").onclick = function () {
+    var hearts = document.getElementById("input_heart").value;
+    var begingame = document.getElementById("begin_game");
+    var score_game = document.getElementById("score");
+    var count_heart = document.getElementById("count_heart");
+    var resulttext = document.getElementById("result_text");
+    var result_game = document.getElementById("result_game");
+    var input_result = document.getElementById("input_result");
+    var button_start = document.getElementById("button_part6");
+    var math = document.getElementById("math");
+    var button_submit = document.getElementById("submit_result");
+    var znaki = ["+", "-", "*", "/"];
+    var score = 0;
+    var timer = true;
+
+    var number_1;
+    var number_2;
+    var znak;
+
+    if (!hearts)
+    {
+        alert("Заполните все поля!");
+    }
+    else if (hearts <= 0)
+    {
+        alert("Введите значение, которое больше нуля.");
+    }
+    else
+    {
+        input_result.disabled = false;
+        button_start.disabled = true;
+        button_submit.disabled = false;
+        start_game();
+        function start_game() {
+            if (hearts > 0 && timer == true) {
+                begingame.innerText = "Игра запущена"
+                score_game.innerText = "Очки: " + score;
+                count_heart.innerText = "Количество жизней: " + hearts;
+                resulttext.innerText = "Введите ответ: "
+                timer = false;
+                if (score >= 0 && score <= 3)
+                {
+                    number_1 = random_chislo(30);
+                    number_2 = random_chislo(30);
+                    znak = znaki[random_chislo(2)];
+                    math.innerText = number_1 + " " + znak + " " + number_2 + " = " + "?";
+                    document.querySelector("#submit_result").onclick = function () {
+                        var input_result_1 = document.getElementById("input_result").value;
+                        if (input_result_1 == get_result(number_1, znak, number_2))
+                        {
+                            result_game.innerText = "Ответ верный. Продолжаем..."
+                            score += 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                        else
+                        {
+                            result_game.innerText = "Ответ не верный. Жизнь потеряна."
+                            hearts -= 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                    }
+                }
+                else if (score > 3 && score <= 7)
+                {
+                    number_1 = random_chislo(40);
+                    number_2 = random_chislo(40);
+                    znak = znaki[random_chislo(2)];
+                    math.innerText = number_1 + " " + znak + " " + number_2 + " = " + "?";
+                    document.querySelector("#submit_result").onclick = function () {
+                        var input_result_1 = document.getElementById("input_result").value;
+                        if (input_result_1 == get_result(number_1, znak, number_2))
+                        {
+                            result_game.innerText = "Ответ верный. Продолжаем..."
+                            score += 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                        else
+                        {
+                            result_game.innerText = "Ответ не верный. Жизнь потеряна."
+                            hearts -= 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                    }
+                }
+                else if (score > 7 && score <= 11)
+                {
+                    number_1 = random_chislo(50);
+                    number_2 = random_chislo(50);
+                    znak = znaki[random_chislo(3)];
+                    math.innerText = number_1 + " " + znak + " " + number_2 + " = " + "?";
+                    document.querySelector("#submit_result").onclick = function () {
+                        var input_result_1 = document.getElementById("input_result").value;
+                        if (input_result_1 == get_result(number_1, znak, number_2))
+                        {
+                            result_game.innerText = "Ответ верный. Продолжаем..."
+                            score += 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                        else
+                        {
+                            result_game.innerText = "Ответ не верный. Жизнь потеряна."
+                            hearts -= 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                    }
+                }
+                else if (score > 11 && score <= 15)
+                {
+                    number_1 = random_chislo(70);
+                    number_2 = random_chislo(70);
+                    znak = znaki[random_chislo(3)];
+                    math.innerText = number_1 + " " + znak + " " + number_2 + " = " + "?";
+                    document.querySelector("#submit_result").onclick = function () {
+                        var input_result_1 = document.getElementById("input_result").value;
+                        if (input_result_1 == get_result(number_1, znak, number_2))
+                        {
+                            result_game.innerText = "Ответ верный. Продолжаем..."
+                            score += 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                        else
+                        {
+                            result_game.innerText = "Ответ не верный. Жизнь потеряна."
+                            hearts -= 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                    }
+                }
+                else
+                {
+                    number_1 = random_chislo(100);
+                    number_2 = random_chislo(100);
+                    znak = znaki[random_chislo(4)];
+                    if (znak == "/")
+                    {
+                        while (number_1 % number_2 != 0 && number_2 != 0)
+                        {
+                            number_1 = random_chislo(100);
+                            number_2 = random_chislo(100);
+                        }
+                    }
+                    start_game();
+                    math.innerText = number_1 + " " + znak + " " + number_2 + " = " + "?";
+                    document.querySelector("#submit_result").onclick = function () {
+                        var input_result_1 = document.getElementById("input_result").value;
+                        if (input_result_1 == get_result(number_1, znak, number_2))
+                        {
+                            result_game.innerText = "Ответ верный. Продолжаем..."
+                            score += 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                        else
+                        {
+                            result_game.innerText = "Ответ не верный. Жизнь потеряна."
+                            hearts -= 1;
+                            timer = true;
+                            start_game();
+                            document.getElementById("input_result").value = "";
+                        }
+                    }
+                }
+            }
+            else if (hearts <= 0)
+            {
+                result_game.innerText = "Ответ не верный. Вы проиграли."
+                begingame.innerText = "Игра окончена."
+                score_game.innerText = "Вы набрали: " + score + " очков.";
+                count_heart.innerText = "";
+                resulttext.innerText = "";
+                math.innerText = "";
+                button_start.disabled = false;
+                button_submit.disabled = true;
+                input_result.disabled = true;
+            }
+        }
+    }
+}
 
 
 
 
-
+function get_result(n1, zn, n2)
+{
+    if (zn == "+")
+    {
+        return n1 + n2;
+    }
+    else if (zn == "-")
+    {
+        return n1 - n2;
+    }
+    else if (zn == "*")
+    {
+        return n1 * n2;
+    }
+    else (zn == "/")
+    {
+        return n1 / n2;
+    }
+}
+function random_chislo(chislo)
+{
+    return Math.floor(Math.random() * chislo);
+}
 
 function truefalse(arr)
 {
-    for (var i = 1; i <= 5; i++)
+    for (var i = 1; i <= 3; i++)
     {
         if (arr[i] % 2 != 0)
         {
